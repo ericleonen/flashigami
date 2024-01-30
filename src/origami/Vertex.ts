05dbbc39df7a4103779803ef9993456de013c34f
ericleonen <ericleonen@gmail.com>
2024-01-30 15:07:45 -0800

    write comments for Vertex class

--- src/origami/Vertex.ts
+++ src/origami/Vertex.ts
@@ -1,12 +1,20 @@
 import Crease from "./Crease";
 import Pair from "./Pair";
 
+/**
+ * Representation of a vertex on a paper that extends Pair. Able to perform operations with
+ * connected creases and other vertexes
+ */
 export default class Vertex extends Pair {
     creases: Set<Crease>;
 
+    /**
+     * Accepts numbers x and y and an optional Set of Crease creases. Initializes a vertex at
+     * (x, y). Initializes creases as the given Set of Crease creases if given, otherwise as an
+     * empty set
+     */
     constructor(x: number, y: number, creases?: Set<Crease>) {
         super(x, y);
-
         this.creases = creases ? creases : new Set();
     }
 }
