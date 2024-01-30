f2dd67a90f09af46fbc196c1a9b8b9095be2e9e3
ericleonen <ericleonen@gmail.com>
2024-01-29 21:10:50 -0800

    update Vector constructor

--- src/origami/Vector.ts
+++ src/origami/Vector.ts
@@ -1,8 +1,18 @@
 import Pair from "./Pair";
+import Vertex from "./Vertex";
 
 export default class Vector extends Pair {
-    constructor(x: number, y: number) {
-        super(x, y);
+    constructor(param1: number | Vertex, param2: number | Vertex) {
+        if (typeof param1 !== typeof param2) {
+            throw new Error("parameters must be the same type")
+        } else if (typeof param1 === "number") {
+            super(param1, param2 as number);
+        } else {
+            param1 = param1 as Vertex;
+            param2 = param2 as Vertex;
+
+            super(param2.x - param1.x, param2.y - param1.y);
+        }
     }
 
     getCopy(): Vector {
